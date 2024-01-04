1a7ca08e49a902e042b849ff425316de9a6a7503
CIRO <cirocavalleggeri@gmail.com>
2024-01-04 12:42:24 +0100

    cooroutine flow viewmodel
    in questo esempio abbiamo adattato la cooroutine flow a compose servendoci di una classe:ViewModel per poter conservare lo stato della cooroutine durante la ricreazione della activity

--- app/src/main/java/org/altervista/ultimaprovaprmadi/composedemo1/viewmodel/MyViewModel.kt
+++ app/src/main/java/org/altervista/ultimaprovaprmadi/composedemo1/viewmodel/MyViewModel.kt
@@ -4,10 +4,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
 
 class MyViewModel:ViewModel() {
     var count by mutableStateOf(0)
-
+    val myFlow = flow<Int> {
+        for (i in 1..100){
+            emit(i)
+            delay(1000L)
+        }
+    }
     fun increaseCount(){
         count++
     }
